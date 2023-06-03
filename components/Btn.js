8752116489e44ac9365a8be4d29d64e77b5579d3
Wanderlee Max <wanderleedev@gmail.com>
2023-06-03 00:03:15 -0500

    archivos base

--- components/Btn.js
+++
@@ -1,9 +0,0 @@
-export const Btn = () => {
-  const tagBtn = document.createElement('button');
-
-  tagBtn.setAttribute('aria-label', 'mostrar o ocultar menu mobile');
-  tagBtn.classList.add('btbMobile');
-  tagBtn.textContent = '☲';
-
-  return tagBtn;
-}
