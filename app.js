9560157bf048c07a5a8ec285f83d2292f1e34ee6
Wanderlee Max <wanderleedev@gmail.com>
2023-06-05 10:45:34 -0500

    Js agregado , page terminada

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import { addBlock } from "./assets/js/utils/addBlock.js";
 import { Header } from "./assets/js/Header.js";
 import { Main } from "./assets/js/Main.js";
+import { Footer } from "./assets/js/Footer.js";
 
-const body = document.querySelector(".page");
-body.insertBefore(Header(), body.firstChild);
-
-console.log(Main());
-//body.innerHTML = Main()
+//header agregado
+const body = document.querySelector(".page"); 
+body.insertAdjacentElement('afterbegin' , Header());
+//main agregado
 addBlock('#app', Main());
+//footer agregado
+addBlock('#app', Footer(),'afterend');
+
+const btnMobile = document.querySelector('.btbMobile');
+
+btnMobile.addEventListener('click', () => {
+  const elementTarget = document.querySelector('.navMobile');
+  console.log(elementTarget);
+  elementTarget.classList.toggle('appearBar');
+})
+
+//element = elementos HTML 
+//HTML = cadenas de texto a formato html
